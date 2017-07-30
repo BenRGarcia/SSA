86838293c302c7970eb5af8fa368cddfb71dd401
SeeBenProgram <ben@SeeBenProgram.com>
2017-07-29 21:05:42 -0400

    Create full_retirement_age function

--- Retirement.py
+++ Retirement.py
@@ -1,7 +1,7 @@
 """ A program that prints a matrix of benefit amounts based on filing month """
 # Run this program in BASH with $ python3 Retirement.py welcome.txt
+import FullRetirementAge
 import datetime
-import time
 #import decimal
 from sys import argv
 script, file_1 = argv
@@ -13,7 +13,7 @@ def introduction(x):
     # Clear the user's terminal screen
     print("\033c")
 
-    # Open welcome.txt, print to terminal
+    # Open .txt file, print to terminal, close .txt file
     open_x = open(x)
     print(open_x.read())
     open_x.close()
@@ -22,7 +22,6 @@ def introduction(x):
     input("\n\nPress the ENTER key to continue\n>>>")
 
 
-# Step 2 - Ascertain the user's date of birth, convert to date object
 def date_of_birth():
     """A function that takes user date of birth input, converts to date object"""
 
@@ -30,21 +29,21 @@ def date_of_birth():
     print("\033c")
 
     # User inputs date of birth (dob), store as variable "dob"
-    dob = input("What is your date of birth? (MM/DD/YYYY)\n>>>")
+    actual_dob = input("What is your date of birth? (MM/DD/YYYY)\n>>>")
 
-    # Exception handling, test user input for valid date
+    # Test user input for valid date format
     try:
-        valid_dob = time.strptime(dob, "%m/%d/%Y")
+        valid_dob = datetime.datetime.strptime(actual_dob, "%m/%d/%Y")
     except ValueError:
-        print("\nSorry, that's not a valid date.")
-        print("\nYou must use the MM/DD/YYYY format.")
+        print("\nSorry, that's not a valid date. You must use the MM/DD/YYYY format.")
         input("\nPress the ENTER key to try again.\n>>>")
         date_of_birth()
 
+    # If no exception, convert user input to date object
     else:
 
         # Convert user input "dob" string into date object "birthdate"
-        birthdate = datetime.datetime.strptime(dob, "%m/%d/%Y").date()
+        birthdate = datetime.datetime.strptime(actual_dob, "%m/%d/%Y").date()
 
         # Subtract 1 day from the actual date of birth b/c of SSA legislative/actuarial rules
         fra_dob = birthdate - datetime.timedelta(days=1)
@@ -52,17 +51,20 @@ def date_of_birth():
         return fra_dob
 
 
+def full_retirement_age(y):
+    """A function that returns the full retirement age (MM/YYYY) according to SSA"""
+
+    print(FullRetirementAge.fra_chart(y))
+
+
 
 # Step 3 - Ascertain the user's "Primary Insurance Amount" according to SSA
 #def ssa_pia():
-    """A function that takes user input of SSA's PIA and converts to decimal object"""
+    #"""A function that takes user input of SSA's PIA and converts to decimal object"""
     # Must use "decimal" module when working with money!!!
 
 
-
 # Step 4 - Take date of birth date object and return full retirement age
-
-
 
 
 # Step 4 - Perform Calculations
@@ -73,18 +75,12 @@ def date_of_birth():
     # Send results to .txt file?
 
 
-
-
-
-
 # Step 5 - Present the benefit estimate in a comprehensible format
 #def benefit_matrix():
     """A function that presents the SSA Retirement benefit matrix"""
 
 
-
-
-
 # Call functions below
-introduction(file_1)
-date_of_birth()
+introduction(file_1)          # Print introduction .txt to terminal
+fra_dob = date_of_birth()     # Call function to get user's date of birth, create global fra_dob
+months_to_fra = full_retirement_age(fra_dob)  # Call function to return user's full retirement age
