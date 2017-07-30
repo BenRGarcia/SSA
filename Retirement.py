fc4994addf6f0676871c70662519b17ebcf31e03
SeeBenProgram <ben@SeeBenProgram.com>
2017-07-30 13:28:18 -0400

    Correction to date_or_birth function

--- Retirement.py
+++ Retirement.py
@@ -28,27 +28,18 @@ def date_of_birth():
     # Clear the user's terminal screen
     print("\033c")
 
-    # User inputs date of birth (dob), store as variable "dob"
-    actual_dob = input("What is your date of birth? (MM/DD/YYYY)\n>>>")
+    # Get user's date of birth
+    dob = input("What is your date of birth? (MM/DD/YYYY)\n>>>")
 
-    # Test user input for valid date of birth date format
+    # Test user input for valid date of birth date format, convert to date object
     try:
-        valid_dob = datetime.datetime.strptime(actual_dob, "%m/%d/%Y")
+        valid_dob = datetime.datetime.strptime(dob, "%m/%d/%Y").date()
+        fra_dob = valid_dob - datetime.timedelta(days=1)
+        return fra_dob # Return date of birth less one day due to SSA regulations
     except:
         print("\nSorry, that's not a valid date. You must use the MM/DD/YYYY format.")
         input("\nPress the ENTER key to try again.\n>>>")
         date_of_birth()
-
-    # If no exception, convert user input to date object
-    else:
-
-        # Convert user input "dob" string into date object "birthdate"
-        dob_date_obj = datetime.datetime.strptime(actual_dob, "%m/%d/%Y")
-
-        # Subtract 1 day from the actual date of birth b/c of SSA legislative/actuarial rules
-        fra_dob = dob_date_obj - datetime.timedelta(days=1)
-
-        return fra_dob
 
 
 def months_to_fra(z): # Apologies, this function is verbose--function modeled after SSA regulations
@@ -162,10 +153,10 @@ def ssa_pia():
 # Call functions below
 introduction(file_1)                     # Print introduction .txt to terminal
 fra_dob = date_of_birth()                # Call function to get user's date of birth, declare fra_dob
-months_to_fra = months_to_fra(fra_dob)   # Call function to return user's # of month until FRA
-fra_month, fra_year = fra_mm_yyyy(months_to_fra) # Call function to return user's FRA (MM/YYYY)
-ssa_pia()
+#months_to_fra = months_to_fra(fra_dob)   # Call function to return user's # of month until FRA
+#fra_month, fra_year = fra_mm_yyyy(months_to_fra) # Call function to return user's FRA (MM/YYYY)
+# ssa_pia()
 
-fra_mm_yy = str(fra_month) + "/" + str(fra_year) # Concatenate string to later convert to date object
-fra_mm_yy_date_obj = datetime.datetime.strptime(fra_mm_yy, "%m/%Y").date()
-print ("According to SSA, your full retirement month and year is:", fra_mm_yy_date_obj.strftime('%B %Y'))
+#fra_mm_yy = str(fra_month) + "/" + str(fra_year) # Concatenate string to later convert to date object
+#fra_mm_yy_date_obj = datetime.datetime.strptime(fra_mm_yy, "%m/%Y").date()
+#print ("According to SSA, your full retirement month and year is:", fra_mm_yy_date_obj.strftime('%B %Y'))
