83a197ae947813e7fbf5df09925969a1595085c1
SeeBenProgram <ben@SeeBenProgram.com>
2017-07-29 20:22:34 -0400

    Write date_of_birth function

--- Retirement.py
+++ Retirement.py
@@ -1,13 +1,13 @@
 """ A program that prints a matrix of benefit amounts based on filing month """
 # Run this program in BASH with $ python3 Retirement.py welcome.txt
-#import datetime
+import datetime
+import time
 #import decimal
 from sys import argv
 script, file_1 = argv
 
 
 def introduction(x):
-
     """A function that introduces the program"""
 
     # Clear the user's terminal screen
@@ -19,27 +19,37 @@ def introduction(x):
     open_x.close()
 
     # Prompt user to continue
-    input("\n\nPress ENTER key to continue\n>>>")
-
+    input("\n\nPress the ENTER key to continue\n>>>")
 
 
 # Step 2 - Ascertain the user's date of birth, convert to date object
-# def date_of_birth():
-
+def date_of_birth():
     """A function that takes user date of birth input, converts to date object"""
 
+    # Clear the user's terminal screen
+    print("\033c")
+
     # User inputs date of birth (dob), store as variable "dob"
-    #dob = input("What is your date of birth? (MM/DD/YYYY) ")
+    dob = input("What is your date of birth? (MM/DD/YYYY)\n>>>")
 
-    # Convert user input "dob" string into date object "birthdate"
-    #birthdate = datetime.datetime.strptime(dob, "%m/%d/%Y").date()
+    # Exception handling, test user input for valid date
+    try:
+        valid_dob = time.strptime(dob, "%m/%d/%Y")
+    except ValueError:
+        print("\nSorry, that's not a valid date.")
+        print("\nYou must use the MM/DD/YYYY format.")
+        input("\nPress the ENTER key to try again.\n>>>")
+        date_of_birth()
 
-    # Subtract 1 day from the actual date of birth b/c of SSA legislative/actuarial rules
-    #fra_dob = birthdate - datetime.timedelta(days=1)
+    else:
 
+        # Convert user input "dob" string into date object "birthdate"
+        birthdate = datetime.datetime.strptime(dob, "%m/%d/%Y").date()
 
+        # Subtract 1 day from the actual date of birth b/c of SSA legislative/actuarial rules
+        fra_dob = birthdate - datetime.timedelta(days=1)
 
-
+        return fra_dob
 
 
 
@@ -76,5 +86,5 @@ def introduction(x):
 
 
 # Call functions below
-
 introduction(file_1)
+date_of_birth()
